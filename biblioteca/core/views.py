b40f7dd1e53ad3044132172329ff9e27439c940c
MarquesFabiano <fabianomarquesjr@gmail.com>
2023-11-08 05:20:06 -0300

    adicionada nova funcionalidade - API de imagens

--- biblioteca/core/views.py
+++ biblioteca/core/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from .forms import LivroForm
 from .models import LivroModel
+import requests
+
+def get_book_cover(book_title):
+    base_url = "https://openlibrary.org/api/books"
+    params = {
+        "bibkeys": f"ISBN:{book_title}",
+        "format": "json",
+        "jscmd": "data"
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        if f"ISBN:{book_title}" in data:
+            cover_url = data[f"ISBN:{book_title}"]["cover"]["large"]
+            return cover_url
+
+    return None
 
 def index(request):
     if request.method == 'GET':
@@ -27,7 +46,11 @@ def listar(request):
         livro_id = request.POST.get('livro_id', '')
         try:
             livro = LivroModel.objects.get(pk=livro_id)
-            contexto = {'livro': livro}
+            
+            book_title = livro.isbn
+            cover_url = get_book_cover(book_title)
+            
+            contexto = {'livro': livro, 'cover_url': cover_url}
         except ValueError:
             contexto = {}
         return render(request, "detalhes.html", contexto)
